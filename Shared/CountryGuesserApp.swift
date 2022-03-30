17b3fd05882a9564e256ceeb8ee186e7f6347b40
Hayden Couch <hcouch@lcs.on.ca>
2022-03-30 10:02:48 -0400

    Initial Commit

---
+++ Shared/CountryGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryGuesserApp.swift
+//  Shared
+//
+//  Created by Hayden Couch on 2022-03-30.
+//
+
+import SwiftUI
+
+@main
+struct CountryGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
